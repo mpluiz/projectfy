0ced835aec86e2da80f16aa1941374e137f22983
mpluiz <marcospaulo.luiz@gmail.com>
2022-06-06 12:14:50 -0300

    feat: typography base component

---
+++ src/components/AppTypography.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: 'AppTypography',
+};
+</script>
+
+<template>
+ <p class="c-typography">
+   <slot></slot>
+ </p>
+</template>
+
+<style scoped lang="scss">
+.c-typography {
+  font-family: Poppins, Roboto, sans-serif;
+  margin: 0;
+}
+</style>
