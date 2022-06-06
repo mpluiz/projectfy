d619f5e5db55d3ec504913385eaa3baa2d21b30b
mpluiz <marcospaulo.luiz@gmail.com>
2022-06-06 12:14:12 -0300

    test: switch button component

---
+++ tests/unit/SwitchButton.spec.js
@@ -0,0 +1,51 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import router from '@/router';
+import SwitchButton from '@/components/SwitchButton.vue';
+
+const localVue = createLocalVue();
+
+function makeComponent(options) {
+  return shallowMount(SwitchButton, { localVue, ...options });
+}
+
+const homeButtonSelector = '[data-testId="switch-button-home"]';
+const listButtonSelector = '[data-testId="switch-button-list"]';
+
+describe('SwitchButton', () => {
+  it('should render component correctly', () => {
+    const wrapper = makeComponent({ router });
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it('should render component with buttons', () => {
+    const wrapper = makeComponent({ router });
+
+    const homeButton = wrapper.find(homeButtonSelector);
+    const listButton = wrapper.find(listButtonSelector);
+
+    expect(homeButton.exists()).toBe(true);
+    expect(listButton.exists()).toBe(true);
+  });
+
+  it('should render component with home button active', () => {
+    router.push('/home');
+    const wrapper = makeComponent({ router });
+
+    const button = wrapper.find(homeButtonSelector);
+
+    expect(button.exists()).toBe(true);
+    expect(button.text()).toBe('home');
+    expect(button.classes()).toContain('c-switch__button--active');
+  });
+
+  it('should render component with list button active', () => {
+    router.push('/list');
+    const wrapper = makeComponent({ router });
+
+    const button = wrapper.find(listButtonSelector);
+
+    expect(button.exists()).toBe(true);
+    expect(button.text()).toBe('list');
+    expect(button.classes()).toContain('c-switch__button--active');
+  });
+});
