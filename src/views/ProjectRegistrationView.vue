608e2d7d2cb0509d9b0c77f204073953d92a55da
mpluiz <marcospaulo.luiz@gmail.com>
2022-06-06 12:31:56 -0300

    feat: project registration view

---
+++ src/views/ProjectRegistrationView.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: 'ProjectRegistration',
+};
+</script>
+
+<template>
+  <div class="project-registration">
+    <h1>Project Registration</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+</style>
