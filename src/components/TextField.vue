093ecc4ea8746c76d1dee30e0219cf5ef0faa1e7
mpluiz <marcospaulo.luiz@gmail.com>
2022-06-06 12:18:52 -0300

    feat: text field base component

---
+++ src/components/TextField.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  name: 'TextField',
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    value: String,
+    hint: String,
+  },
+};
+</script>
+
+<template>
+  <v-text-field
+    :label="label"
+    :value="value"
+    :hint="hint"
+    outlined
+  ></v-text-field>
+</template>
+
+<style scoped lang="scss">
+
+</style>
