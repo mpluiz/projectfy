7872eb7103b50bcc52945922ffe31d6c2b57ff30
mpluiz <marcospaulo.luiz@gmail.com>
2022-06-06 13:23:24 -0300

    feat: app card component

---
+++ src/components/AppCard.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: 'AppCard',
+};
+</script>
+
+<template>
+  <div class="c-appcard">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.c-appcard {
+  width: 68%;
+  height: 60%;
+  padding: 32px;
+  background: #FFFFFF;
+  box-shadow: 0 4px 35px rgb(0 0 0 / 8%);
+  border-radius: 40px;
+}
+</style>
