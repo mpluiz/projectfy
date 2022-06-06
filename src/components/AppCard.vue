9772931f52a3fcdd8a77cf4f805604a198c7b2fc
mpluiz <marcospaulo.luiz@gmail.com>
2022-06-06 16:44:31 -0300

    refactor: app card component with css vars

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -14,9 +14,9 @@ export default {
 .c-appcard {
   width: 68%;
   height: 60%;
-  padding: 32px;
-  background: #FFFFFF;
-  box-shadow: 0 4px 35px rgb(0 0 0 / 8%);
-  border-radius: 40px;
+  padding: var(--space-xs);
+  background: var(--color-bright);
+  box-shadow: 0 var(--space-quark) var(--space-xs) rgb(0 0 0 / 8%);
+  border-radius: var(--space-sm);
 }
 </style>
