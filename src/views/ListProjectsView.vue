94ac87b6c257baa54e14a255b89eb6354bb884cb
mpluiz <marcospaulo.luiz@gmail.com>
2022-06-06 12:32:05 -0300

    feat: list projects view

---
+++ src/views/ListProjectsView.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: 'ListProjects',
+};
+</script>
+
+<template>
+  <div class="project-registration">
+    <h1>List Projects</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+</style>
