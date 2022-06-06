8b32d6dcce99ee5ce834f006ed4629815d58f761
mpluiz <marcospaulo.luiz@gmail.com>
2022-06-06 12:14:03 -0300

    feat: switch button component

---
+++ src/components/SwitchButton.vue
@@ -0,0 +1,67 @@
+<script>
+import AppTypography from '@/components/AppTypography.vue';
+
+export default {
+  name: 'SwitchButton',
+  components: { AppTypography },
+  data() {
+    return {
+      routes: [
+        { label: 'home', path: '/home' },
+        { label: 'list', path: '/list' },
+      ],
+    };
+  },
+  methods: {
+    isActive(param) {
+      return this.$route.name === param;
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="c-switch">
+    <template v-for="route in routes">
+      <router-link
+        :key="route.label"
+        :id="'switch-button-' + route.label"
+        :data-testId="'switch-button-' + route.label"
+        :class="{'c-switch__button--active': isActive(route.label) }"
+        class="c-switch__button"
+        :to="route.path"
+      >
+        <AppTypography>{{ route.label }}</AppTypography>
+      </router-link>
+    </template>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.c-switch {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 360px;
+  height: 60px;
+  background: #D0C4EE;
+  border-radius: 33px;
+  padding: 12px;
+
+  &__button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #fff;
+    text-decoration: none;
+    text-transform: capitalize;
+    width: 160px;
+    height: 40px;
+
+    &--active {
+      background: #8C75E1;
+      border-radius: 33px;
+    }
+  }
+}
+</style>
