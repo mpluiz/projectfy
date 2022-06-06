815a7246af30a8322586b09935b9324aa8427ecb
mpluiz <marcospaulo.luiz@gmail.com>
2022-06-05 23:07:27 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import HomeView from '@/views/HomeView.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HomeView,
+  },
+};
+</script>
+
+<template>
+  <HomeView />
+</template>
