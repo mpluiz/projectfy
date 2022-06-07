d316ca6c828206976c485603b02554a8bedb243c
mpluiz <marcospaulo.luiz@gmail.com>
2022-06-07 15:36:55 -0300

    feat: date input component

---
+++ src/components/DateInput.vue
@@ -0,0 +1,62 @@
+<script>
+export default {
+  name: 'DateInput',
+  props: {
+    value: {
+      type: undefined,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+    min: {
+      type: String,
+    },
+  },
+  data() {
+    return {
+      menu: false,
+      currentValue: this.value,
+    };
+  },
+
+  methods: {
+    handleInput(event) {
+      this.$emit('input', event);
+    },
+
+    handleDatePicker(event) {
+      this.menu = false;
+      this.$emit('input', event);
+    },
+  },
+};
+</script>
+
+<template>
+  <v-menu
+    v-model="menu"
+    :close-on-content-click="false"
+    transition="scale-transition"
+    offset-y
+    min-width="auto"
+  >
+    <template v-slot:activator="{ on, attrs }">
+      <v-text-field
+        :label="label"
+        readonly
+        outlined
+        v-bind="attrs"
+        v-on="on"
+        :value="value"
+        @input="handleInput">
+      ></v-text-field>
+    </template>
+    <v-date-picker
+      v-model="currentValue"
+      @input="handleDatePicker"
+      :min="min"
+      no-title
+    ></v-date-picker>
+  </v-menu>
+</template>
