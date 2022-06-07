d025fb04983b640fdf11077a162ee58ced26fa8f
mpluiz <marcospaulo.luiz@gmail.com>
2022-06-07 15:04:41 -0300

    feat: add overflow to app card component

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -18,6 +18,7 @@ export default {
   background: var(--color-bright);
   box-shadow: 0 var(--space-quark) var(--space-xs) rgb(0 0 0 / 8%);
   border-radius: var(--space-sm);
+  overflow: auto;
   z-index: 1;
 }
 </style>
