ee45d68ee9e6137b31de5333554805181b324ced
mpluiz <marcospaulo.luiz@gmail.com>
2022-06-07 13:27:52 -0300

    feat: app button component

---
+++ src/components/AppButton.vue
@@ -0,0 +1,134 @@
+<script>
+import AppTypography from '@/components/AppTypography.vue';
+import Loading from '@/assets/loading.svg';
+
+export default {
+  name: 'AppButton',
+  components: { AppTypography },
+  props: {
+    type: {
+      type: String,
+      required: true,
+    },
+    variant: {
+      type: String,
+      default: 'primary',
+    },
+    large: {
+      type: Boolean,
+      default: false,
+    },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    fullWidth: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  data() {
+    return {
+      contentLoading: Loading,
+    };
+  },
+
+  computed: {
+    handleClass() {
+      return [
+        'c-button',
+        `c-button--${this.variant}`,
+        { 'c-button--large': this.large },
+        { 'c-button--disabled': this.disabled },
+        { 'c-button--full-width': this.fullWidth },
+      ];
+    },
+  },
+};
+</script>
+
+<template>
+  <button
+   :class="handleClass"
+   :type="type"
+   :disabled="disabled"
+  >
+    <AppTypography
+      variant="body"
+      weight="semi-bold"
+      :color="variant === 'default' ? 'dark' : 'bright'"
+    >
+      <img v-if="loading" class="c-button__loading" :src="contentLoading" alt="loading" />
+      <slot v-if="!loading"></slot>
+    </AppTypography>
+  </button>
+</template>
+
+<style scoped lang="scss">
+.c-button {
+  $self: &;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-width: var(--space-xl);
+  min-height: var(--space-sm);
+  background: var(--color-primary);
+  border-radius: var(--space-nano);
+  opacity: 1;
+  transition: opacity 0.3s;
+
+  &:active:not(&--disabled) {
+    opacity: 0.7;
+    transition: opacity 0s ease-in;
+  }
+
+  &__loading {
+    height: var(--space-xxs);
+  }
+
+  &--primary {
+    background: var(--color-primary);
+  }
+
+  &--secondary {
+    background: var(--color-secondary);
+  }
+
+  &--default {
+    background: var(--color-default);
+  }
+
+  &--success {
+    background: var(--color-success);
+  }
+
+  &--warning {
+    background: var(--color-warning);
+  }
+
+  &--danger {
+    background: var(--color-danger);
+  }
+
+  &--large {
+    height: var(--space-lg);
+
+    #{$self}__loading {
+      height: var(--space-sm);
+    }
+  }
+
+  &--disabled {
+    background: var(--color-secondary);
+  }
+
+  &--full-width {
+    width: 100%;
+  }
+}
+</style>
