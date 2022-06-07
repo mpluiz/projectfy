e0615e28ecfa49b750b03215f9bc0c7297ea7c50
mpluiz <marcospaulo.luiz@gmail.com>
2022-06-07 13:47:24 -0300

    test: app button component

---
+++ tests/unit/components/AppButton.spec.js
@@ -0,0 +1,90 @@
+import { shallowMount } from '@vue/test-utils';
+import AppButton from '@/components/AppButton.vue';
+
+function makeComponent(options) {
+  return shallowMount(AppButton, {
+    propsData: {
+      type: 'button',
+    },
+    ...options,
+  });
+}
+
+describe('AppButton', () => {
+  it('should render component correctly', () => {
+    const wrapper = makeComponent();
+
+    expect(wrapper.exists()).toBe(true);
+    expect(wrapper.classes()).toContain('c-button');
+  });
+
+  it('should render component with correct children', () => {
+    const wrapper = makeComponent({ slots: { default: 'valid_param' } });
+
+    expect(wrapper.html()).toContain('valid_param');
+  });
+
+  it('should render component with correct default props', () => {
+    const wrapper = makeComponent();
+
+    expect(wrapper.classes()).toContain('c-button');
+    expect(wrapper.classes()).toContain('c-button--primary');
+  });
+
+  it('should render component with submit type', () => {
+    const wrapper = makeComponent({
+      propsData: {
+        type: 'submit',
+      },
+    });
+
+    expect(wrapper.attributes('type')).toBe('submit');
+  });
+
+  it('should render component with fullWidth modifier', () => {
+    const wrapper = makeComponent({
+      propsData: {
+        type: 'button',
+        fullWidth: true,
+      },
+    });
+
+    expect(wrapper.classes()).toContain('c-button--full-width');
+  });
+
+  it('should render component with the disabled attribute and modifier', () => {
+    const wrapper = makeComponent({
+      propsData: {
+        type: 'button',
+        disabled: true,
+      },
+    });
+
+    expect(wrapper.attributes('disabled')).toBe('disabled');
+    expect(wrapper.classes()).toContain('c-button--disabled');
+  });
+
+  it('should render component with the large modifier', () => {
+    const wrapper = makeComponent({
+      propsData: {
+        type: 'button',
+        large: true,
+      },
+    });
+
+    expect(wrapper.classes()).toContain('c-button--large');
+  });
+
+  it('should render component with the loading mode', () => {
+    const wrapper = makeComponent({
+      propsData: {
+        type: 'button',
+        loading: true,
+      },
+    });
+
+    const loadingElement = wrapper.find('.c-button__loading');
+
+    expect(loadingElement.exists()).toBe(true);
+  });
+});
