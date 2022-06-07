36d53e9ec44ba6fa024d84719d5426f5e31455cf
mpluiz <marcospaulo.luiz@gmail.com>
2022-06-07 17:09:46 -0300

    test: date input component

---
+++ tests/unit/components/VDateInput.spec.js
@@ -0,0 +1,26 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+import Vuetify from 'vuetify';
+import VDateInput from '@/components/VDateInput.vue';
+
+const localVue = createLocalVue();
+const vuetify = new Vuetify();
+
+function makeComponent(options) {
+  return shallowMount(VDateInput, {
+    localVue,
+    vuetify,
+    propsData: {
+      id: 'valid_id',
+      label: 'valid_label',
+    },
+    ...options,
+  });
+}
+
+describe('VDateInput', () => {
+  it('should render component correctly', () => {
+    const wrapper = makeComponent();
+
+    expect(wrapper.exists()).toBe(true);
+  });
+});
