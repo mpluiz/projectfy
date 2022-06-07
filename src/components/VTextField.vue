f47bc46ebcacf916167e43b3f0413e1ccaf1c42d
mpluiz <marcospaulo.luiz@gmail.com>
2022-06-07 17:10:02 -0300

    feat: test field component

---
+++ src/components/VTextField.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  name: 'VTextField',
+  props: {
+    value: {
+      type: undefined,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+
+  methods: {
+    handleInput(event) {
+      this.$emit('input', event);
+    },
+  },
+};
+</script>
+
+<template>
+  <v-text-field
+    :label="label"
+    :value="value"
+    @input="handleInput"
+    outlined
+  ></v-text-field>
+</template>
