51f0a6ab551e9b9f84832f0db8ec5ef49515021e
mpluiz <marcospaulo.luiz@gmail.com>
2022-06-08 14:38:13 -0300

    feat: list project view

--- src/views/ListProjectsView.vue
+++ src/views/ListProjectsView.vue
@@ -1,14 +1,38 @@
 <script>
+import VSwitchButton from '@/components/VSwitchButton.vue';
+import VContent from '@/components/VContent.vue';
+import VCard from '@/components/VCard.vue';
+import VTypography from '@/components/VTypography.vue';
+
 export default {
   name: 'ListProjects',
+  components: {
+    VTypography, VSwitchButton, VContent, VCard,
+  },
 };
 </script>
 
 <template>
-  <div class="project-registration">
-    <h1>List Projects</h1>
-  </div>
+  <VContent>
+    <VCard>
+      <div class="list-projects">
+        <div class="header">
+          <VTypography variant="title" weight="semi-bold">List</VTypography>
+          <VSwitchButton />
+        </div>
+
+      </div>
+    </VCard>
+  </VContent>
 </template>
 
 <style scoped lang="scss">
+.list-projects {
+
+  .header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+}
 </style>
