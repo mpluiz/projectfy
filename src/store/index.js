4a4b1ae2b84c820d0e81fe6b215428e7283f0c52
mpluiz <marcospaulo.luiz@gmail.com>
2022-06-08 21:25:22 -0300

    feat: vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -5,13 +5,18 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    projects: JSON.parse(localStorage.getItem('projects') || '[]'),
   },
   getters: {
+    projects: (state) => state.projects,
   },
   mutations: {
+    addProject(state, project) {
+      localStorage.removeItem('projects');
+      state.projects.push(project);
+      localStorage.setItem('projects', JSON.stringify(state.projects));
+    },
   },
-  actions: {
-  },
-  modules: {
-  },
+  actions: {},
+  modules: {},
 });
